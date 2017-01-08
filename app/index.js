cbaa4f53bca5f88adadf60e12b7f04c751418557
febeck <fe.beck25@gmail.com>
2017-01-08 12:13:11 +0100

    Create common folder App

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class App extends Component {
+	render(){
+		return(
+			<Text> Hello World! </Text>
+		)
+	}
+}
+
+export default App;
