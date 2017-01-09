0d02f67a52198d911d10f1a53c925f0d20d58525
febeck <fe.beck25@gmail.com>
2017-01-09 09:41:41 +0100

    Authenticate user for login and signup

--- app/routes/Authentication.js
+++ app/routes/Authentication.js
@@ -1,20 +1,87 @@
 import React, {Component} from 'react';
 import {
+  AsyncStorage,
+  Alert,
   Text,
   TextInput,
   TouchableOpacity,
   View
 } from 'react-native';
+import {Actions} from 'react-native-router-flux';
 import styles from './styles';
+
+const STORAGE_KEY = 'id_token';
 
 class Authentication extends Component {
 
   constructor(){
     super();
     this.state = {
-      email: null,
+      username: null,
       password: null
     }
+  }
+
+  async onValueChange(item, selectedValue) {
+      try {
+        await AsyncStorage.setItem(item, selectedValue);
+      } catch (error) {
+        console.log('AsyncStorage error: ' + error.message);
+      }
+  }
+
+  userSignup() {
+  if (this.state.username && this.state.password) {
+    // TODO: localhost doesn't work. Get the IP address with ifconfig.
+    fetch("http://147.250.223.228:3001/users", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.onValueChange(STORAGE_KEY, responseData.id_token),
+      Alert.alert(
+        "Signup Success!",
+        "Click the button to get a Chuck Norris quote!"
+      )
+    })
+    .done();
+  }
+  Actions.HomePage();
+}
+
+  userLogin() {
+    if (this.state.username && this.state.password) {
+      // TODO: localhost doesn't work. Get the IP address with ifconfig.
+      fetch("http://147.250.223.228:3001/sessions/create", {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password,
+        })
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.onValueChange(STORAGE_KEY, responseData.id_token),
+        Alert.alert(
+          "Login Success!",
+          "Click the button to get a Chuck Norris quote!"
+        )
+      })
+      .done();
+    }
+    Actions.HomePage();
   }
 
   render() {
@@ -26,12 +93,12 @@ class Authentication extends Component {
         <View style={styles.form}>
           <TextInput
             editable={true}
-            onChangeText={(email) => this.setState({email})}
-            placeholder='Email'
-            ref='email'
+            onChangeText={(username) => this.setState({username})}
+            placeholder='Username'
+            ref='username'
             returnKeyType='next'
             style={styles.inputText}
-            value={this.state.email}
+            value={this.state.username}
           />
           <TextInput
             editable={true}
@@ -43,13 +110,19 @@ class Authentication extends Component {
             style={styles.inputText}
             value={this.state.password}
           />
-          <TouchableOpacity style={styles.buttonWrapper}>
+          <TouchableOpacity
+            style={styles.buttonWrapper}
+            onPress={this.userLogin.bind(this)}
+          >
             <Text style={styles.buttonText}>
               Log In
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.buttonWrapper}>
-            <Text style={styles.buttonText} >
+          <TouchableOpacity
+            style={styles.buttonWrapper}
+            onPress={this.userSignup.bind(this)}
+          >
+            <Text style={styles.buttonText}>
               Sign Up
             </Text>
           </TouchableOpacity>
